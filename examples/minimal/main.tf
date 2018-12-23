5e9d8db44ca42a8e5ea4a3ea3dd38850a1ed6fc6
tmknom <tmk.nom@gmail.com>
2018-12-23 20:10:32 +0900

    Implement minimal example

--- examples/minimal/main.tf
+++ examples/minimal/main.tf
@@ -1,1 +1,13 @@
+module "codebuild" {
+  source              = "../../"
+  name                = "example"
+  artifact_bucket_arn = "${aws_s3_bucket.artifact.arn}"
+}
 
+resource "aws_s3_bucket" "artifact" {
+  bucket        = "artifact-codebuild-${data.aws_caller_identity.current.account_id}"
+  acl           = "private"
+  force_destroy = true
+}
+
+data "aws_caller_identity" "current" {}
