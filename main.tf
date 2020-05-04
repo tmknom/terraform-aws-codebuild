6656edef56a57fbf949251a2b4d00cf58f4f5735
tmknom <tmk.nom@gmail.com>
2020-05-04 09:49:04 +0900

    Upgraded module to support Terraform 0.12

--- main.tf
+++ main.tf
@@ -4,9 +4,9 @@
 
 # https://www.terraform.io/docs/providers/aws/r/codebuild_project.html
 resource "aws_codebuild_project" "default" {
-  name         = "${var.name}"
-  description  = "${var.description}"
-  service_role = "${aws_iam_role.default.arn}"
+  name         = var.name
+  description  = var.description
+  service_role = aws_iam_role.default.arn
 
   # Information about the build output artifacts for the build project.
   # https://docs.aws.amazon.com/codebuild/latest/APIReference/API_ProjectArtifacts.html
@@ -19,7 +19,7 @@ resource "aws_codebuild_project" "default" {
   environment {
     # The type of build environment to use for related builds.
     # Available values are: LINUX_CONTAINER or WINDOWS_CONTAINER.
-    type = "${var.environment_type}"
+    type = var.environment_type
 
     # Information about the compute resources the build project uses. Available values include:
     #
@@ -28,17 +28,17 @@ resource "aws_codebuild_project" "default" {
     # - BUILD_GENERAL1_LARGE: Use up to 15 GB memory and 8 vCPUs for builds.
     #
     # BUILD_GENERAL1_SMALL is only valid if type is set to LINUX_CONTAINER
-    compute_type = "${var.compute_type}"
+    compute_type = var.compute_type
 
     # The image identifier of the Docker image to use for this build project.
     # https://docs.aws.amazon.com/codebuild/latest/userguide/build-env-ref-available.html
-    image = "${var.image}"
+    image = var.image
 
     # Enables running the Docker daemon inside a Docker container.
     # Set to true only if the build project is be used to build Docker images,
     # and the specified build environment image is not provided by AWS CodeBuild with Docker support.
     # Otherwise, all associated builds that attempt to interact with the Docker daemon fail.
-    privileged_mode = "${var.privileged_mode}"
+    privileged_mode = var.privileged_mode
   }
 
   # Information about the build input source code for the build project.
@@ -50,7 +50,7 @@ resource "aws_codebuild_project" "default" {
     # If you include a build spec as part of the source code, by default,
     # the build spec file must be named buildspec.yml and placed in the root of your source directory.
     # https://docs.aws.amazon.com/codebuild/latest/userguide/build-spec-ref.html#build-spec-ref-name-storage
-    buildspec = "${var.buildspec}"
+    buildspec = var.buildspec
   }
 
   # Information about the cache for the build project.
@@ -60,13 +60,13 @@ resource "aws_codebuild_project" "default" {
     #
     # - NO_CACHE: The build project does not use any cache.
     # - S3: The build project reads and writes from and to S3.
-    type = "${var.cache_type}"
+    type = var.cache_type
 
     # Information about the cache location:
     #
     # - NO_CACHE: This value is ignored.
     # - S3: This is the S3 bucket name/prefix.
-    location = "${var.cache_location}"
+    location = var.cache_location
   }
 
   # The KMS customer master key (CMK) to be used for encrypting the build output artifacts.
@@ -75,14 +75,14 @@ resource "aws_codebuild_project" "default" {
   #
   # If set empty string, CodeBuild uses the AWS-managed CMK for Amazon S3 in your AWS account.
   # https://docs.aws.amazon.com/codebuild/latest/userguide/setting-up.html#setting-up-kms
-  encryption_key = "${var.encryption_key}"
+  encryption_key = var.encryption_key
 
   # How long in minutes, from 5 to 480 (8 hours), for AWS CodeBuild to wait until timing out
   # any related build that does not get marked as completed.
-  build_timeout = "${var.build_timeout}"
+  build_timeout = var.build_timeout
 
   # A mapping of tags to assign to the resource.
-  tags = "${merge(map("Name", var.name), var.tags)}"
+  tags = merge({ "Name" = var.name }, var.tags)
 }
 
 # CodeBuild Service Role
@@ -91,11 +91,11 @@ resource "aws_codebuild_project" "default" {
 
 # https://www.terraform.io/docs/providers/aws/r/iam_role.html
 resource "aws_iam_role" "default" {
-  name               = "${local.iam_name}"
-  assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
-  path               = "${var.iam_path}"
-  description        = "${var.description}"
-  tags               = "${merge(map("Name", local.iam_name), var.tags)}"
+  name               = local.iam_name
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+  path               = var.iam_path
+  description        = var.description
+  tags               = merge({ "Name" = local.iam_name }, var.tags)
 }
 
 data "aws_iam_policy_document" "assume_role_policy" {
@@ -111,10 +111,10 @@ data "aws_iam_policy_document" "assume_role_policy" {
 
 # https://www.terraform.io/docs/providers/aws/r/iam_policy.html
 resource "aws_iam_policy" "default" {
-  name        = "${local.iam_name}"
-  policy      = "${data.aws_iam_policy_document.policy.json}"
-  path        = "${var.iam_path}"
-  description = "${var.description}"
+  name        = local.iam_name
+  policy      = data.aws_iam_policy_document.policy.json
+  path        = var.iam_path
+  description = var.description
 }
 
 data "aws_iam_policy_document" "policy" {
@@ -128,7 +128,7 @@ data "aws_iam_policy_document" "policy" {
     ]
 
     resources = [
-      "${local.log_group_arn}",
+      local.log_group_arn,
       "${local.log_group_arn}:*",
     ]
   }
@@ -143,7 +143,7 @@ data "aws_iam_policy_document" "policy" {
     ]
 
     resources = [
-      "${var.artifact_bucket_arn}",
+      var.artifact_bucket_arn,
       "${var.artifact_bucket_arn}/*",
     ]
   }
@@ -151,26 +151,26 @@ data "aws_iam_policy_document" "policy" {
 
 # https://www.terraform.io/docs/providers/aws/r/iam_role_policy_attachment.html
 resource "aws_iam_role_policy_attachment" "default" {
-  role       = "${aws_iam_role.default.name}"
-  policy_arn = "${aws_iam_policy.default.arn}"
+  role       = aws_iam_role.default.name
+  policy_arn = aws_iam_policy.default.arn
 }
 
 # ECR provides several managed policies that you can attach to IAM users or EC2 instances
 # that allow differing levels of control over Amazon ECR resources and API operations.
 # https://docs.aws.amazon.com/AmazonECR/latest/userguide/ecr_managed_policies.html
 resource "aws_iam_role_policy_attachment" "ecr" {
-  count = "${var.enabled_ecr_access}"
+  count = var.enabled_ecr_access
 
-  role       = "${aws_iam_role.default.name}"
-  policy_arn = "${var.ecr_access_policy_arn}"
+  role       = aws_iam_role.default.name
+  policy_arn = var.ecr_access_policy_arn
 }
 
 locals {
   iam_name      = "${var.name}-codebuild"
   log_group_arn = "arn:aws:logs:${local.region}:${local.account_id}:log-group:/aws/codebuild/${var.name}"
 
-  region     = "${data.aws_region.current.name}"
-  account_id = "${data.aws_caller_identity.current.account_id}"
+  region     = data.aws_region.current.name
+  account_id = data.aws_caller_identity.current.account_id
 }
 
 data "aws_caller_identity" "current" {}
