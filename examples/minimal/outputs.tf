6595066716b24da95a4bc44644bdac38522e2b02
tmknom <tmk.nom@gmail.com>
2020-05-04 09:51:24 +0900

    Upgraded examples/minimal to support Terraform 0.12

--- examples/minimal/outputs.tf
+++ examples/minimal/outputs.tf
@@ -1,51 +1,51 @@
 output "codebuild_project_id" {
-  value = "${module.codebuild.codebuild_project_id}"
+  value = module.codebuild.codebuild_project_id
 }
 
 output "codebuild_project_arn" {
-  value = "${module.codebuild.codebuild_project_arn}"
+  value = module.codebuild.codebuild_project_arn
 }
 
 output "iam_role_arn" {
-  value = "${module.codebuild.iam_role_arn}"
+  value = module.codebuild.iam_role_arn
 }
 
 output "iam_role_create_date" {
-  value = "${module.codebuild.iam_role_create_date}"
+  value = module.codebuild.iam_role_create_date
 }
 
 output "iam_role_unique_id" {
-  value = "${module.codebuild.iam_role_unique_id}"
+  value = module.codebuild.iam_role_unique_id
 }
 
 output "iam_role_name" {
-  value = "${module.codebuild.iam_role_name}"
+  value = module.codebuild.iam_role_name
 }
 
 output "iam_role_description" {
-  value = "${module.codebuild.iam_role_description}"
+  value = module.codebuild.iam_role_description
 }
 
 output "iam_policy_id" {
-  value = "${module.codebuild.iam_policy_id}"
+  value = module.codebuild.iam_policy_id
 }
 
 output "iam_policy_arn" {
-  value = "${module.codebuild.iam_policy_arn}"
+  value = module.codebuild.iam_policy_arn
 }
 
 output "iam_policy_description" {
-  value = "${module.codebuild.iam_policy_description}"
+  value = module.codebuild.iam_policy_description
 }
 
 output "iam_policy_name" {
-  value = "${module.codebuild.iam_policy_name}"
+  value = module.codebuild.iam_policy_name
 }
 
 output "iam_policy_path" {
-  value = "${module.codebuild.iam_policy_path}"
+  value = module.codebuild.iam_policy_path
 }
 
 output "iam_policy_document" {
-  value = "${module.codebuild.iam_policy_document}"
+  value = module.codebuild.iam_policy_document
 }
